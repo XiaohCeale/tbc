2bda938a0c916e23b054971ecf90005094d43ebb
XiaohCeale <3050155211@qq.com>
2023-04-16 02:43:33 +0800

    new file:   group/ceale.jpg 	new file:   group/chickroom.jpg 	new file:   group/hatsuneylk.jpg 	new file:   group/yuer.jpg 	new file:   img1.png 	modified:   index.html 	modified:   script.js 	modified:   style.css 	modified:   index.html

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function logoImg() {
     let fileName = [
         '北屿',
         '村庄',
@@ -11,8 +11,62 @@ $(function(){
         '迎新广场',
         'Wry'
     ];
-    console.log(fileName)
     let src = 'url(/background/' + fileName[Math.floor(Math.random() * fileName.length)] + '.png)'
-    console.log(src)
-    $('#st1').css('background-image', src)
+    $('#st1 .background').css('background-image', src)
+})
+
+$(function hoverCard() {
+    let timer = null
+    $('#nd2 .server-card .child-server-card').mousemove(
+        function(e) {
+            if (timer !== null) return
+            timer = setTimeout(()=>{
+                x = $(this).offset().left
+                y = $(this).offset().top
+                mx = e.pageX
+                my = e.pageY
+                w = $(this).innerWidth()
+                h = $(this).innerHeight()
+                rotateX = (my - y) / h - 0.5
+                rotateY = (mx - x) / w - 0.5
+                $(this).css('--rotateX', rotateX * -2 + 'deg')
+                $(this).css('--rotateY', rotateY * 2 + 'deg')
+                timer = null
+            }, 50)
+        }
+    )
+})
+
+$(function load() {
+    console.log()
+    console.log(
+        '\n' + '%c 渐蓝方块' + '%c TwoBlueCube' + '%c \n',
+        'color: #fff; background: #169c9c; padding: 5px;',
+        'color: #fff; background: #3ab3da; padding: 5px;',
+        'background: transparent;'
+    )
+    if (Math.floor(Math.random()*101)==100) console.log('这里是彩蛋QWQ')
+})
+
+// $(function scroll() {
+//     let timer = null
+//     let scroll
+//     $(window).scroll(function () {
+//         if (timer !== null) return
+//         timer = setTimeout(()=>{
+//             scroll = $(document).scrollTop();
+//             $('#st1 .background').css('transform', `translateY(${ 0.4 * scroll }px)`)
+//             timer = null
+//             console.log(0)
+//         }, 5)
+// });
+// })
+
+
+$(window).scroll(function () {
+    scroll = $(document).scrollTop()
+    height = $(window).height()
+    if ( scroll > height ) return
+    rate = 0.5
+    $('#st1 .background').css('transform', 'translateY('+ rate * scroll +'px)')
 })
